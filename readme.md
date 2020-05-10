75a294dedb7c0bcfb2535477ea17fe54772e4d36
King Wanyama <kingwanyama01@gmail.com>
2020-05-10 14:20:56 +0300

    Fully working with stripe payment gateway

---
+++ readme.md
@@ -0,0 +1,1 @@
+King Wanyama ecommerce site
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Created by  King Wanyama</div>
+{{--        <i class="fa fa-heart heart"></i>--}}
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
